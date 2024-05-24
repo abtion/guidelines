053be9671f1748ba3942a17c4130044b8125ac53
Markus Obelitz <mo@abtion.com>
2024-05-24 10:49:58 +0200

    CI: Add action to update "Last updated" timestamp

---
+++ update_timestamps.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+from git import Repo # type: ignore
+
+# Path to the repository
+repo = Repo(os.getcwd())
+# Get the list of changed files
+changed_files_paths = [item.a_path for item in repo.index.diff(None) if item.a_path.endswith('.md')]
+
+# Define the timestamp format
+timestamp_format = "%Y-%m-%d"
+
+def update_timestamp(file_path):
+  with open(file_path, 'r') as file:
+    content = file.readlines()
+
+  timestamp_added = False
+  with open(file_path, 'w') as file:
+    for line in content:
+      if line.startswith("Last updated:"):
+        file.write(f"Last updated: {datetime.now().strftime(timestamp_format)}\n")
+
+        timestamp_added = True
+      else:
+        file.write(line)
+
+    if not timestamp_added:
+      file.write(f"\nLast updated: {datetime.now().strftime(timestamp_format)}\n")
+
+for file_path in changed_files_paths:
+  update_timestamp(file_path)
